166bbffd52255bdd09d8d95269d83eb1d18d77dc
Jacobo Martinez <jacobo.martinez@mediamonks.com>
2019-12-27 16:16:49 -0300

    add button component for last example

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -52,6 +52,7 @@ export default HomePage;
 //     return (
 //       <div className="component-wrapper">
 //         <h1 className="title">Using static contextType</h1>
+
 //         <div className="card">
 //           {isVisible ? (
 //             <img className="gif" src={url} alt="Cat" />
@@ -62,6 +63,7 @@ export default HomePage;
 //               </span>
 //             </p>
 //           )}
+
 //           <button
 //             type="button"
 //             className="button"
@@ -86,6 +88,7 @@ export default HomePage;
 //   return (
 //     <div className="component-wrapper">
 //       <h1 className="title">Using hooks</h1>
+
 //       <div className="card">
 //         {isVisible ? (
 //           <img className="gif" src={url} alt="Cat" />
@@ -96,6 +99,7 @@ export default HomePage;
 //             </span>
 //           </p>
 //         )}
+
 //         <button
 //           type="button"
 //           className="button"
@@ -103,6 +107,36 @@ export default HomePage;
 //         >
 //           {isVisible ? `close` : `show me`}
 //         </button>
+//       </div>
+//     </div>
+//   );
+// };
+// export default HomePage;
+
+// === Functional 2 === //
+// import React, { useContext } from 'react';
+// import GifContext from '../context/GifContext';
+// import GenerateGifButton from './GenerateGifButton';
+
+// const HomePage = () => {
+//   const { url, isVisible } = useContext(GifContext);
+
+//   return (
+//     <div className="component-wrapper">
+//       <h1 className="title">Using hooks</h1>
+
+//       <div className="card">
+//         {isVisible ? (
+//           <img className="gif" src={url} alt="Cat" />
+//         ) : (
+//           <p className="subtitle">
+//             <span className="emoji" role="img" aria-label="Cat">
+//               🐈
+//             </span>
+//           </p>
+//         )}
+
+//         <GenerateGifButton />
 //       </div>
 //     </div>
 //   );
